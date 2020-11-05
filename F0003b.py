2155965f9c46f4da90513706e47ad4837d512fea
Lopatovszki <55076178+Lopatovszki@users.noreply.github.com>
2020-11-05 22:19:09 +0100

    Add files via upload

---
+++ F0003b.py
@@ -0,0 +1,9 @@
+vezetéknév = input(' Mi a vezetékneved? ')
+keresztnév = input(' Mi a keresztneved? ')
+születési_év = input( 'Mi a születési éved? ')
+csillagjegy = input(' Mi a csillagjegyed? ')
+
+print(' A te neved ' + vezetéknév , keresztnév +'.')
+print(' A születési éved: ' + születési_év)
+print(' A te csillagjegyed: ' + csillagjegy)
+
